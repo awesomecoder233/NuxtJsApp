d02fea59e7b9d0c842803122157f96db267e30ee
awesomecoder233 <tommount58@gmail.com>
2021-09-09 17:46:13 +0200

    define css style and install node-sass loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/myFile.css'
+    //load a Node.js directory (here is a sass file)
+    //~'bulma',
+    //CSS file in the project
+    '@/assets/css/main.css',
+    //SCSS file in the project
+    //'@/assets/css/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
